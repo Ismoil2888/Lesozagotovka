776e70a49a3f0a471267f5909729f3e00afb610b
Ismoil Hamidzoda <ismoil2888@gmail.com>
2024-02-18 19:17:38 +0500

    Обновление поля для отправки заявок в нижней части сайта.

--- js/email.js
+++ js/email.js
@@ -4,12 +4,13 @@ document.getElementById('submitBtn').addEventListener('click', function(event) {
     let emailInput = document.getElementById('email');
     let email = emailInput.value.trim();
 
-    if (email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailRegex.test(email)) {
         localStorage.setItem('userEmail', email);
         alert('Вы отправили заявку, ожидайте в скором времени мы с вами свяжемся!');
-
         emailInput.value = '';
     } else {
-        alert('Введите адрес электронной почты!');
+        alert('Введите корректный адрес электронной почты!');
     }
 });
